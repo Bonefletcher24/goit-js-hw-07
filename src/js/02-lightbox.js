3956a450900cc97ab28a9faf1ff2213ead8e8e32
Ayten <139014081+Bonefletcher24@users.noreply.github.com>
2024-03-09 14:17:56 +0400

    upd

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
@@ -20,5 +20,8 @@ function createGalleryItem(array) {
 const photosMarkup = createGalleryItem(galleryItems);
 galleryContainter.insertAdjacentHTML("beforeend", photosMarkup);
 
-const galleryHandler = new SimpleLightbox(".gallery a", { captionsData: "alt", captionsDelay: 250 });
-galleryHandler.on("show.simplelightbox");
+const galleryHandler = new SimpleLightbox(".gallery a", { 
+    captionsData: "alt", 
+    captionsDelay: 250 
+});
+galleryHandler.on("show.SimpleLightbox");
